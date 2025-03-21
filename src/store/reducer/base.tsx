116bb08a8f56a6ed8911c1fa6609d7a4182b5e58
xiangcc <xiangcc@smartebao.com>
2025-03-21 15:30:09 +0800

    1

--- src/store/reducer/base.tsx
+++ src/store/reducer/base.tsx
@@ -9,11 +9,9 @@ export default function baseReducer(state = initial, action) {
     state = JSON.parse(JSON.stringify(state))
     switch(action.type) {
         case TYPES.VOTE_SUP:
-            console.log(1111)
             state.supNum++;
             break;
         case TYPES.VOTE_OPP:
-            console.log(22)
             state.oppNum++;
             break;
         default:
